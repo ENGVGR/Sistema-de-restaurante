fa30199165b21c152bdb4c340ed622f0799118cd
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-08-30 16:52:31 -0300

    Teste do metodo EditName para a classe costumer

--- src/classes/costumer/costumer.test.js
+++ src/classes/costumer/costumer.test.js
@@ -1,35 +1,54 @@
 import Costumer from './costumer';
-// const Costumer = require('./costumer');
 
 const costumer1 = new Costumer('Alice', 1, 'Pedido em andamento');
 const costumer2 = new Costumer('Pedro', 2, 'Aguardando atendimento');
 const costumer3 = new Costumer('Fernanda', 3, 'Pedido entregue');
 const costumer4 = new Costumer('Felipe', 4, 'Finalizado');
 
-test('Get costumer1 information', () => {
+test('Get costumer information', () => {
   expect(costumer1.GetCostumer()).toStrictEqual([
     'Alice',
     1,
     'Pedido em andamento',
   ]);
-});
 
-test('Get costumer2 information', () => {
   expect(costumer2.GetCostumer()).toStrictEqual([
     'Pedro',
     2,
     'Aguardando atendimento',
   ]);
-});
 
-test('Get costumer3 information', () => {
   expect(costumer3.GetCostumer()).toStrictEqual([
     'Fernanda',
     3,
     'Pedido entregue',
   ]);
+
+  expect(costumer4.GetCostumer()).toStrictEqual(['Felipe', 4, 'Finalizado']);
 });
 
-test('Get costumer4 information', () => {
+test('Edit costumer name', () => {
+  costumer1.EditName('Gabriela');
+  expect(costumer1.GetCostumer()).toStrictEqual([
+    'Gabriela',
+    1,
+    'Pedido em andamento',
+  ]);
+
+  costumer2.EditName('Vinícius');
+  expect(costumer2.GetCostumer()).toStrictEqual([
+    'Vinícius',
+    2,
+    'Aguardando atendimento',
+  ]);
+
+  costumer3.EditName('Geovana');
+  expect(costumer3.GetCostumer()).toStrictEqual([
+    'Geovana',
+    3,
+    'Pedido entregue',
+  ]);
+
+  costumer3.EditName('Daniel');
   expect(costumer4.GetCostumer()).toStrictEqual(['Felipe', 4, 'Finalizado']);
 });
