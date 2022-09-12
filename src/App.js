bd759a8320ed01ac41c7e641b23b0c8da2a4d82b
wallacelw <70167391+wallacelw@users.noreply.github.com>
2022-09-11 23:35:26 -0300

    app.js revert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 import Menu from './pages/menu';
-import Stock from './pages/stock';
 
 function App() {
-  // return <Menu />;
-  return <Stock />;
+  return <Menu />;
 }
 
 export default App;
